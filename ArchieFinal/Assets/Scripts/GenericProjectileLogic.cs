806956bf9e5500950424022a3d6fe1c57e168fad
Thomas <tjk@san.rr.com>
2016-11-27 17:43:03 -0800

    Nintendo hates us

---
+++ ArchieFinal/Assets/Scripts/GenericProjectileLogic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericProjectileLogic : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        var hit = other.gameObject;
+        var healthScript = hit.GetComponent<PlayerHealth>();
+        if (healthScript != null)
+        {
+            healthScript.TakeDamage(20);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ ArchieFinal/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerHealth : NetworkBehaviour {
+
+    public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    public bool isDead;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void TakeDamage(int damageAmt)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= damageAmt;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            currentHealth = maxHealth;
+            transform.position = new Vector3(500, 500, 500);
+            RpcRespawn();
+        }
+    }
+
+    void OnChangeHealth(int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            StartCoroutine(RespawnWithDelay(1));
+        }
+    }
+
+     IEnumerator RespawnWithDelay(float delay)
+    {
+        if (isLocalPlayer)
+        {
+            yield return new WaitForSeconds(delay);
+            transform.position = Vector3.zero;
+        }
+    }
+}
