ccbaca8c955079e751774b024e68d4c3a79d5219
Will Anderson <andersow@usc.edu>
2016-11-29 01:46:25 -0800

    itchi.io build

--- ArchieFinal/Assets/Scripts/PlayerHealth.cs
+++ ArchieFinal/Assets/Scripts/PlayerHealth.cs
@@ -36,24 +36,6 @@ public class PlayerHealth : NetworkBehaviour {
             currentHealth = 0;
             currentHealth = maxHealth;
             transform.position = new Vector3(500, 500, 500);
-            ScoreManager gameScore = FindObjectOfType<ScoreManager>();
-            //if you are light unity chan (server and local player or client and not local player)
-            if ((isServer && isLocalPlayer) || (!isServer && !isLocalPlayer))
-            {
-                //if the score manager exists
-                if (gameScore != null) {
-                    //add score for light unitychan
-                    gameScore.IncrementLightScore();
-                }
-            }
-            //else do it for dark
-            else {
-                //if the score manager exists
-                if (gameScore != null) {
-                    //add score for light unitychan
-                    gameScore.IncrementDarkScore();
-                }
-            }
             RpcRespawn();
         }
     }
@@ -69,6 +51,23 @@ public class PlayerHealth : NetworkBehaviour {
         if (isLocalPlayer)
         {
             StartCoroutine(RespawnWithDelay(1));
+        }
+        ScoreManager gameScore = FindObjectOfType<ScoreManager>();
+        //if you are light unity chan (server and local player or client and not local player)
+        if ((isServer && isLocalPlayer) || (!isServer && !isLocalPlayer)) {
+            //if the score manager exists
+            if (gameScore != null) {
+                //add score for dark unitychan
+                gameScore.IncrementDarkScore();
+            }
+        }
+        //else do it for dark
+        else {
+            //if the score manager exists
+            if (gameScore != null) {
+                //add score for light unitychan
+                gameScore.IncrementLightScore();
+            }
         }
     }
 
